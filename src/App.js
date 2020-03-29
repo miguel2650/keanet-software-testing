c66c17b69c3cdb12c84a57cfd179b480c3ca5429
Mikkel Madsen <slaske.madsen@gmail.com>
2020-03-29 15:22:49 +0200

    changes to cellphonetable

--- src/App.js
+++ src/App.js
@@ -1,51 +1,62 @@
-import React, { useState } from 'react';
-import { Container, Col, Button, Row } from "react-bootstrap"
-import InternetCheckbox from "./components/InternetCheckbox"
-import PhoneLinesInput from "./components/PhoneLinesInput"
-import CellPhonesTable from './components/CellPhonesTable';
+import React, { useState } from "react";
+import { Container, Col, Button, Row } from "react-bootstrap";
+import InternetCheckbox from "./components/InternetCheckbox";
+import PhoneLinesInput from "./components/PhoneLinesInput";
+import CellPhonesTable from "./components/CellPhonesTable";
 
 function App() {
   // State controlling internet checkbox
-  const [internetState, setInternetState] = useState(false)
-  const [phoneLinesState, setPhoneLinesState] = useState(0)
+  const [internetState, setInternetState] = useState(false);
+  const [phoneLinesState, setPhoneLinesState] = useState(0);
   const [cellPhonesState, setCellPhonesState] = useState([
     {
-      name:"Motorola G99",
-      value:0
+      name: "Motorola G99",
+      value: 0
     },
     {
-      name:"iPhone 99",
-      value:0
+      name: "iPhone 99",
+      value: 0
     },
     {
-      name:"Samsung Galaxy 99",
-      value:0
+      name: "Samsung Galaxy 99",
+      value: 0
     },
     {
-      name:"Sony Xperia 99",
-      value:0
+      name: "Sony Xperia 99",
+      value: 0
     },
     {
-      name:"Huawei 99",
-      value:0
+      name: "Huawei 99",
+      value: 0
     }
-  ])
+  ]);
 
   return (
-    <Container >
+    <Container>
       <Col lg="10" md="8" sm="6">
-      <h1>KEANet</h1>
-      <Row>
-      <InternetCheckbox internetState={internetState} setInternetState={setInternetState}></InternetCheckbox>
-      </Row>
-      <Row>
-      <PhoneLinesInput phoneLinesState={phoneLinesState} setPhoneLinesState={setPhoneLinesState}></PhoneLinesInput>
-      </Row>
-      <Row>
-        <CellPhonesTable cellPhonesState={cellPhonesState} setCellPhonesState={setCellPhonesState}></CellPhonesTable>
-      </Row>
+        <h1>KEANet</h1>
+        <Row>
+          <InternetCheckbox
+            internetState={internetState}
+            setInternetState={setInternetState}
+          ></InternetCheckbox>
+        </Row>
+        <Row>
+          <PhoneLinesInput
+            phoneLinesState={phoneLinesState}
+            setPhoneLinesState={setPhoneLinesState}
+          ></PhoneLinesInput>
+        </Row>
+        <Row>
+          <CellPhonesTable
+            cellPhonesState={cellPhonesState}
+            setCellPhonesState={setCellPhonesState}
+          ></CellPhonesTable>
+        </Row>
       </Col>
-      <Button onClick={() =>console.log(phoneLinesState)}>Test state of phone</Button>
+      <Button onClick={() => console.log(phoneLinesState)}>
+        Test state of phone
+      </Button>
     </Container>
   );
 }
--- src/components/CellPhonesTable.js
+++ src/components/CellPhonesTable.js
@@ -1,78 +1,92 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const phonesList = [
-    {
-      name:"Motorola G99",
-      value:0
-    },
-    {
-      name:"iPhone 99",
-      value:0
-    },
-    {
-      name:"Samsung Galaxy 99",
-      value:0
-    },
-    {
-      name:"Sony Xperia 99",
-      value:0
-    },
-    {
-      name:"Huawei 99",
-      value:0
-    }
-  ]
+  {
+    name: "Motorola G99",
+    value: 0
+  },
+  {
+    name: "iPhone 99",
+    value: 0
+  },
+  {
+    name: "Samsung Galaxy 99",
+    value: 0
+  },
+  {
+    name: "Sony Xperia 99",
+    value: 0
+  },
+  {
+    name: "Huawei 99",
+    value: 0
+  }
+];
 
 function CellPhonesTable(props) {
+  const [selectedPhone, setSelectedPhone] = useState();
 
-    const [selectedPhone, setSelectedPhone] = useState()
+  // Takes the parents passed in function (setState)
+  // and changes the state based on the current state.
+  const handleChange = e => {
+    setSelectedPhone(e.target.value);
+    console.log(selectedPhone);
+  };
 
-    // Takes the parents passed in function (setState)
-    // and changes the state based on the current state.
-    const handleChange = e => {
-        setSelectedPhone(e.target.value)
-        console.log(selectedPhone)
-    }
+  const handleAdd = e => {
+    console.log(selectedPhone, "selected phone");
+    // Never update state directly.
+    console.log(e.target.value);
+    const newState = [...props.cellPhonesState];
+    newState[0].value = newState[0].value + 1;
+    props.setCellPhonesState(newState);
+    console.log(props.cellPhonesState);
+  };
 
-    const handleAdd = e => {
-        props.setCellPhonesState(props.cellPhonesState[selectedPhone].value + 1)
-    }
-
-
-    return (
-        <React.Fragment>
-            <label>Cell phones</label>
-            <table>
-             <tbody>
-                 <tr>
-                 <td>
-                        <select size={props.cellPhonesState.length} id="txtCellPhones" value={selectedPhone} onChange={handleChange}>
-                        {props.cellPhonesState.map((phone,index) => 
-                                <option key={phone.name+index} value={phone.name}>{phone.name}</option>
-                            )}
-                        </select>
-                            </td>
-                            <td>
-
-                            <button onClick={handleAdd}>&gt;</button>
-                            <br></br>
-                            <button>&lt;</button>
-                            </td>
-                            <select>
-                            {props.cellPhonesState.map((phone,index) => {
-                                if (phone.value < 0) {
-                                    return (
-                                        <option key={phone.name+index} value={phone.name}>{phone.name}</option>
-                                        )
-                                }
-                            }
-                            )}
-                            </select>
-                            </tr>
-                            </tbody>
-            </table>
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <label>Cell phones</label>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <select
+                size={props.cellPhonesState.length}
+                id="txtCellPhones"
+                onChange={handleChange}
+              >
+                {props.cellPhonesState.map((phone, index) => (
+                  <option
+                    key={phone.name + index}
+                    value={phone.name}
+                    onClick={() => setSelectedPhone(index)}
+                  >
+                    {phone.name}
+                  </option>
+                ))}
+              </select>
+            </td>
+            <td>
+              <button onClick={handleAdd}>&gt;</button>
+              <br></br>
+              <button>&lt;</button>
+            </td>
+            <select>
+              {props.cellPhonesState.map((phone, index) => {
+                if (phone.value < 0) {
+                  return (
+                    <option key={phone.name + index} value={phone.name}>
+                      {phone.name}
+                    </option>
+                  );
+                }
+              })}
+            </select>
+          </tr>
+        </tbody>
+      </table>
+    </React.Fragment>
+  );
 }
 
-export default CellPhonesTable
+export default CellPhonesTable;
