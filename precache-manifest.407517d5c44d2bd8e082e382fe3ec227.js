161b9ee31a9a5eb8fbf087871ffca2b1b2a9168e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-03 07:45:54 +0000

    Deploy miguel2650/keanet-software-testing to github.com/miguel2650/keanet-software-testing.git:gh-pages

---
+++ precache-manifest.407517d5c44d2bd8e082e382fe3ec227.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c611f0867533c5ed7b30b24a14c1b88d",
+    "url": "/keanet-software-testing/index.html"
+  },
+  {
+    "revision": "99b653c6aaf115d802f7",
+    "url": "/keanet-software-testing/static/css/main.05fe4cf7.chunk.css"
+  },
+  {
+    "revision": "2e99b028b3ff70345ddf",
+    "url": "/keanet-software-testing/static/js/2.c5158431.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/keanet-software-testing/static/js/2.c5158431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99b653c6aaf115d802f7",
+    "url": "/keanet-software-testing/static/js/main.dd2a74b0.chunk.js"
+  },
+  {
+    "revision": "4144abfc55ce958a03af",
+    "url": "/keanet-software-testing/static/js/runtime-main.a60f878f.js"
+  }
+]);
